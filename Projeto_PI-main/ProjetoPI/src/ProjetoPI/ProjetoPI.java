c37dd4d52aa7e657a28b6cbe65a7492d6f6ba81a
Empty17 <132923260+Empty17@users.noreply.github.com>
2023-09-21 19:59:28 -0300

    Add files via upload

---
+++ Projeto_PI-main/ProjetoPI/src/ProjetoPI/ProjetoPI.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ProjetoPI;
+
+/**
+ *
+ * @author KaueR
+ */
+public class ProjetoPI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ Projeto_PI-main/README.md
@@ -0,0 +1,4 @@
+# Projeto_PI
+Espaço dedicado para salvamento da construção do projeto integrador!
+
+Realizaremos atualizações todas as vezes em que o projeto for evoluindo!
